952dde2eebf780ffc4278c08a76c50767c05a8ab
Fernando Santos <fernando.wdesign@icloud.com>
2015-05-15 11:55:32 -0300

    Ajuste no Javascript

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,4 @@
-var banners = [
-	"Os melhores do Brasil!", "Qualidade e preço baixo!"
-];
+var banners = ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
 var bannerAtual = 0;
 
 function trocaBanner(){
